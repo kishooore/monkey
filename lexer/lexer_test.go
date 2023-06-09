625e8173e5fd83a01723fdbceaa6b2a765bcfc9a
kishore <kishore@cloudera.com>
2023-06-09 12:38:22 +0530

    add support for conditional statements

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -142,3 +142,46 @@ func TestNextToken_WithMathOperators(t *testing.T) {
 		}
 	}
 }
+
+func TestNextToken_WithConditionalStatements(t *testing.T) {
+	input := `if (5 < 10) {
+	return true;
+} else {
+	return false;
+}`
+	l := New(input)
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+	}
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
